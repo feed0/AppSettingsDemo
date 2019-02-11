7cc83b9849025cab3ccceb86d729c9d71f8bee49
Karoly Nyisztor <karolynyisztor@car-booth-25-mac.content.linkedin.biz>
2019-02-11 12:34:35 -0800

    Initial Commit

---
+++ AppSettingsDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AppSettingsDemo
+//
+//  Created by Karoly Nyisztor on 2/11/19.
+//  Copyright © 2019 Karoly Nyisztor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
